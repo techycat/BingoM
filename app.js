dd0a912017cc2d90f0bee03a60f6ac75346c7dd9
nik <nik@adobe.com>
2018-01-27 17:08:39 +0530

    changes

--- app.js
+++ app.js
@@ -5,8 +5,19 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8000;
 var server_ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.get('/', function(req, res) {
-	res.end('Hello Nithin');
+	var sum=0
+	for(var i=1;i<100000;i++) {
+		sum=sum+i;
+		setTimeout(test,15000);
+		if(sum>1000){
+			sum=sum%1000;
+		}
+	}
+	res.end('Hello Nithin '+sum);
 });
+function test() {
+	console.log("Testing");
+}
 
 app.listen(server_port,server_ip, function() {
 	// body...
