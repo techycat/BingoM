6972c008341932422e83393a77397ad24efa9b6d
techycat <techycat7@gmail.com>
2018-01-27 18:01:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8000;
-var server_ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+console.log("Port : "+process.env.OPENSHIFT_NODEJS_IP);
 
 app.get('/', function(req, res) {
 	var sum=0
@@ -19,7 +21,7 @@ function test() {
 	console.log("Testing");
 }
 
-app.listen(server_port,server_ip, function() {
+app.listen(server_port,server_ip_address, function() {
 	// body...
 	console.log("Listening on "+server_ip+" port : "+server_port);
 });
